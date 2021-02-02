5e002e1106c185367dde3efa4423e4fc16dcea27
anzileiro <anderson.anzileiro@gmail.com>
2021-02-01 23:48:12 -0300

    chore: update repo pre video

--- src/application/creational/factory-method/AccountFactory.ts
+++
@@ -1,5 +0,0 @@
-import Account from '@application/creational/factory-method/model/Account';
-
-export default abstract class AccountFactory {
-    public abstract getAccount(identifier: string | number): Account;
-}
--- src/application/creational/factory-method/BusinessFactory.ts
+++
@@ -1,9 +0,0 @@
-import Business from '@application/creational/factory-method/model/Business';
-import AccountFactory from '@application/creational/factory-method/AccountFactory';
-import Account from '@application/creational/factory-method/model/Account';
-
-export default class BusinessFactory extends AccountFactory {
-  public getAccount(identifier: number): Account {
-    return new Business(identifier);
-  }
-}
